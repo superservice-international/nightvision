4f208bfee86c69b5bf8d76339015b21eb5a65ec2
Christoph Reimers <christoph@superservice-international.com>
2015-03-29 18:27:43 +0200

    cosmetics

--- take_picture.py
+++ take_picture.py
@@ -4,13 +4,14 @@ import datetime
 
 
 def take_picture():
+    now = datetime.datetime.now()
+    now_str = now.strftime('%Y-%m-%d-%H:%M:%S')
+    file_name = now_str + '.jpg'
     with picamera.PiCamera() as camera:
-        now = datetime.datetime.now()
-        now_str = now.strftime('%Y-%m-%d-%H:%M:%S')
         camera.resolution = (1024, 768)
         time.sleep(2)
-        file_name = now_str + '.jpg'
         camera.capture(file_name)
+        print (file_name + " taken")
         return file_name
 
 
