24c89d208269c3fc63540c775f68756c0e91aca3
creimers <christoph@superservice-international.com>
2015-10-25 18:43:05 +0100

    what do I know

--- take_picture.py
+++ take_picture.py
@@ -4,11 +4,12 @@ import datetime
 import os
 import requests
 from requests.auth import HTTPBasicAuth
+from PIL import Image
 
 
 def take_picture():
     now = datetime.datetime.now()
-    now_str = now.strftime('%Y-%m-%d-%H:%M:%S')
+    now_str = now.strftime('%Y-%m-%d_%H-%M-%S')
     path = os.path.dirname(os.path.abspath(__file__))
     file_name = path + "/" + now_str + '.jpg'
     with picamera.PiCamera() as camera:
@@ -19,16 +20,19 @@ def take_picture():
         return file_name
 
 
+def reduce_filezise(file_name):
+    im = Image.open(file_name)
+    im.save(file_name, 'JPEG', quality=90)
+
+
 def post_picture(file_name):
-    host = 'http://192.168.178.32:8000/pictures/'
-    user = os.environ["PIC_USER"]
-    pw = os.environ["PIC_USER_PW"]
+    host = 'http://kamera.reimers-sportpferde.de/-/pictures/'
+    user = "creimers"
+    pw = "1lb2es!M"
     print ("now posting " + file_name)
     post = requests.post(url=host, auth=HTTPBasicAuth(user, pw), files={'photo': open(file_name, 'rb')})
     if post.status_code == 201:
         return True
-    else:
-        return False
 
 
 def delete_picture(file_name):
@@ -36,8 +40,19 @@ def delete_picture(file_name):
     os.remove(file_name)
 
 
+def post_remaining():
+    path = os.path.dirname(os.path.abspath(__file__))
+    for pic in os.listdir(path):
+        if pic.split('.')[-1] == 'jpg':
+            post = post_picture('/'.join([path, pic]))
+            if post:
+                delete_picture('/'.join([path, pic]))
+
+
 if __name__ == "__main__":
     picture = take_picture()
+    reduce_filezise(picture)
     posted = post_picture(picture)
     if posted:
         delete_picture(picture)
+    post_remaining()
